d9f0b8cb34b5a3cea067532506dbe670cae5ca47
slycolon <jasonphillips130@gmail.com>
2018-04-25 10:16:50 -0500

    Update speed.cpp

--- speed.cpp
+++ speed.cpp
@@ -6,5 +6,16 @@ using namespace std;
 
 int main()
 {
-
+  cout<<"welcome to speed checker. this is a list of speeds (mph) and the time it takes to go one mile (in minutes)."
+    <<endl<<endl
+    <<"speed(mph)		time(minutes)";
+  
+  for (int x = 150; x > 0; x--)
+  {
+    cout<<endl
+      << x << "		" << x/60;
+  }
+  
+  system("PAUSE");
+  return 0;
 }
